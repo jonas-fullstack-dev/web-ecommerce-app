5561e301c5ee5fcca2a165432f8aa32b1908c15a
jonas-fullstack-dev <jonas.furrer@hotmail.com>
2022-05-17 09:12:59 +0200

    Initial Check-in

---
+++ src/test/java/ch/simplenio/springbootecommerce/SpringBootEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.simplenio.springbootecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
