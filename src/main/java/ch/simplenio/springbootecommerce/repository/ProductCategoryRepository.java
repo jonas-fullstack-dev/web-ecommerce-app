52639f435a22f9ace77f0b045e59e9d3cc4a6410
jonas-fullstack-dev <jonas.furrer@hotmail.com>
2022-06-06 15:18:03 +0200

    Add cross origin support

--- src/main/java/ch/simplenio/springbootecommerce/repository/ProductCategoryRepository.java
+++ src/main/java/ch/simplenio/springbootecommerce/repository/ProductCategoryRepository.java
@@ -2,12 +2,14 @@ package ch.simplenio.springbootecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import ch.simplenio.springbootecommerce.entity.ProductCategory;
 
 /**
  * @author jfurrer
  */
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/ch/simplenio/springbootecommerce/repository/ProductRepository.java
+++ src/main/java/ch/simplenio/springbootecommerce/repository/ProductRepository.java
@@ -2,11 +2,13 @@ package ch.simplenio.springbootecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import ch.simplenio.springbootecommerce.entity.Product;
 
 /**
  * @author jfurrer
  */
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
